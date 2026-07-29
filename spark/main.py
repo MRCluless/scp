b620d9dbae60993ed6bddb9fe1ecb3fb9ef3f225
Bhavik Padam Chand <bhavikjainsmb@outlook.com>
2026-07-29 10:46:11 +0100

    merged sanket

--- spark/main.py
+++ spark/main.py
@@ -1,77 +1,5 @@
 from pyspark.sql import SparkSession
-<<<<<<< HEAD
 from pyspark.sql.functions import avg, round, input_file_name, regexp_extract
-=======
-
-from pyspark.sql.functions import (
-    col,
-    avg,
-    max as spark_max,
-    min as spark_min,
-    sum as spark_sum,
-    count,
-    countDistinct,
-    when,
-    current_timestamp,
-    stddev,
-    round as spark_round
-)
-
-from pyspark.sql.types import (
-    StructType,
-    StructField,
-    StringType,
-    IntegerType,
-    DoubleType,
-    BooleanType,
-    TimestampType
-)
-
-from pyspark import StorageLevel
-
-from pathlib import Path
-
-import os
-import time
-import platform
-import builtins
-
-
-os.environ["PYSPARK_PYTHON"] = "python"
-
-os.environ["PYSPARK_DRIVER_PYTHON"] = "python"
-
-
-
-
-spark_temp_dir = r"C:\SparkTemp"
-
-
-if not os.path.exists(spark_temp_dir):
-    os.makedirs(spark_temp_dir)
-
-
-os.environ["TMPDIR"] = spark_temp_dir
-os.environ["TEMP"] = spark_temp_dir
-os.environ["TMP"] = spark_temp_dir
-
-
-
-os.environ["HADOOP_HOME"] = r"C:\hadoop"
-
-
-os.environ["PATH"] = (
-    r"C:\hadoop\bin"
-    +
-    os.pathsep
-    +
-    os.environ.get(
-        "PATH",
-        ""
-    )
-)
-
->>>>>>> 3d18d55783a9a47196a1592378dd3b5279b24494
 
 def main():
     spark = SparkSession.builder \
@@ -111,14 +39,5 @@ def main():
     batch_view.write.mode("overwrite").json(s3_output_path)
     spark.stop()
 
-<<<<<<< HEAD
-=======
-
-
-
-
-
->>>>>>> 3d18d55783a9a47196a1592378dd3b5279b24494
 if __name__ == "__main__":
-
     main()
